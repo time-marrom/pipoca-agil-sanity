aa2585bc43a95683363896be9895884597db9a28
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-15 21:12:20 -0300

    refactor: Update fields for home page

--- schemas/home.ts
+++ schemas/home.ts
@@ -4,157 +4,200 @@ export default {
   title: 'Início',
   description: 'Conteúdos da página Home',
   fields: [
+    // HERO
     {
-      name: 'title',
+      name: 'heroTitle',
       type: 'string',
       title: 'Título',
       description: 'Texto que ficará em evidência na página.',
     },
     {
-      name: 'description',
+      name: 'heroSubtitle',
       type: 'string',
-      title: 'Descrição',
+      title: 'Subtítulo',
       description: 'Texto que dará contexto a página.',
     },
     {
-      name: 'imageURL',
-      type: 'url',
-      title: 'Imagem URL',
-      description: 'URL da imagem que ficará em evidência na página.',
-    },
-    {
-      name: 'imageAlt',
-      type: 'string',
-      title: 'Imagem Alt',
-      description: 'Texto alternativo para a imagem em evidência na página.',
-    },
-    {
-      name: 'heroButtonLabel',
+      name: 'heroLabelButton',
       type: 'string',
       title: 'Label do botão do Hero',
       description: 'Label do botão do Hero.',
     },
+    // DESCRIPTION
     {
-      name: 'heroTopics',
+      name: 'descriptionTitle',
+      type: 'string',
+      title: 'Título do painel "O que é?"',
+      description: 'Título do painel "O que é?"',
+    },
+    {
+      name: 'descriptionFirstParagraph',
+      type: 'string',
+      title: 'Primeiro parágrafo do painel "O que é?"',
+      description: 'Primeiro parágrafo do painel "O que é?"',
+    },
+    {
+      name: 'descriptionSecondParagraph',
+      type: 'string',
+      title: 'Segundo parágrafo do painel "O que é?"',
+      description: 'Segundo parágrafo do painel "O que é?"',
+    },
+    // HOW IT WORKS
+    {
+      name: 'howItWorksTitle',
+      type: 'string',
+      title: 'Título do painel "Como funciona?"',
+      description: 'Título do painel "Como funciona?"',
+    },
+    {
+      name: 'howItWorksSubtitle',
+      type: 'string',
+      title: 'Subtítulo do painel "Como funciona?"',
+      description: 'Subtítulo do painel "Como funciona?"',
+    },
+    {
+      name: 'howItWorksTopics',
       type: 'array',
-      title: 'Tópicos do Hero',
-      description: 'Tópicos que descrevem o conteúdo da podcast.',
-      of: [{type: 'string'}],
+      title: 'Tópicos do painel "Como funciona?"',
+      description: 'Tópicos do painel "Como funciona?"',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
     },
+    // WHO IS IT FOR?
     {
-      name: 'simulationSectionTitle',
+      name: 'howIsItForTitle',
       type: 'string',
-      title: 'Título da seção de simulação',
-      description: 'Título da seção de simulação.',
+      title: 'Título do painel "Posso me inscrever?"',
+      description: 'Título do painel "Posso me inscrever?"',
     },
     {
-      name: 'simulationSectionDescription',
+      name: 'howIsItForSubtitle',
       type: 'string',
-      title: 'Descrição da seção de simulação',
-      description: 'Descrição da seção de simulação.',
+      title: 'Subtítulo do painel "Posso me inscrever?"',
+      description: 'Subtítulo do painel "Posso me inscrever?"',
     },
     {
-      name: 'simulationSectionTopics',
+      name: 'howIsItForVolunteerText',
+      type: 'string',
+      title: 'Texto para voluntários',
+      description: 'Texto para voluntários',
+    },
+    {
+      name: 'howIsItForVolunteerLabelButton',
+      type: 'string',
+      title: 'Label do botão para voluntários',
+      description: 'Label do botão para voluntários',
+    },
+    {
+      name: 'howIsItForMentorText',
+      type: 'string',
+      title: 'Texto para mentores',
+      description: 'Texto para mentores',
+    },
+    {
+      name: 'howIsItForMentorLabelButton',
+      type: 'string',
+      title: 'Label do botão para mentores',
+      description: 'Label do botão para mentores',
+    },
+    // WORTH IT?
+    {
+      name: 'worthItTitle',
+      type: 'string',
+      title: 'Título do painel "Vale a pena?"',
+      description: 'Título do painel "Vale a pena?"',
+    },
+    {
+      name: 'worthItTopics',
       type: 'array',
-      title: 'Tópicos da seção de simulação',
-      description: 'Tópicos da seção de simulação.',
-      of: [{type: 'string'}],
+      title: 'Tópicos do painel "Vale a pena?"',
+      description: 'Tópicos do painel "Vale a pena?"',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'number',
+              type: 'number',
+              title: 'Chave',
+              description: 'Chave do objeto.',
+            },
+            {
+              name: 'metric',
+              type: 'string',
+              title: 'Valor',
+              description: 'Valor do objeto.',
+            },
+          ],
+        },
+      ],
+    },
+    // DEPOSITIONS
+    {
+      name: 'depositionsTitle',
+      type: 'string',
+      title: 'Título do painel "Depoimentos"',
+      description: 'Título do painel "Depoimentos"',
     },
     {
-      name: 'simulationSectionMemberButtonLabel',
+      name: 'depositionsTopics',
+      type: 'array',
+      title: 'Depoimentos de quem já participou',
+      description: 'Depoimentos de quem já participou',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'deposition',
+              type: 'string',
+              title: 'Depoimento',
+              description: 'Depoimento.',
+            },
+            {
+              name: 'author',
+              type: 'string',
+              title: 'Autor',
+              description: 'Autor do depoimento.',
+            },
+          ],
+        },
+      ],
+    },
+    // FAQ
+    {
+      name: 'faqTitle',
       type: 'string',
-      title: 'Label do botão da seção de simulação para voluntário',
-      description: 'Label do botão da seção de simulação para voluntário.',
+      title: 'Título do painel "Perguntas frequentes"',
+      description: 'Título do painel "Perguntas frequentes"',
     },
     {
-      name: 'simulationSectionMentorButtonLabel',
-      type: 'string',
-      title: 'Label do botão da seção de simulação para mentor',
-      description: 'Label do botão da seção de simulação para mentor.',
-    },
-    {
-      name: 'podcastSectionTitle',
-      type: 'string',
-      title: 'Título da seção de podcast',
-      description: 'Título da seção de podcast.',
-    },
-    {
-      name: 'podcastSectionDescription',
-      type: 'string',
-      title: 'Descrição da seção de podcast',
-      description: 'Descrição da seção de podcast.',
-    },
-    {
-      name: 'popcornClubSectionTitle',
-      type: 'string',
-      title: 'Título da seção clube do pipoca',
-      description: 'Título da seção clube do pipoca.',
-    },
-    {
-      name: 'popcornClubSectionDescription',
-      type: 'string',
-      title: 'Descrição da seção clube do pipoca',
-      description: 'Descrição da seção clube do pipoca.',
-    },
-    {
-      name: 'popcornClubSectionButtonLabel',
-      type: 'string',
-      title: 'Label do botão da seção clube do pipoca',
-      description: 'Label do botão da seção clube do pipoca.',
-    },
-    {
-      name: 'agileTrailSectionTitle',
-      type: 'string',
-      title: 'Título da seção de trilha ágil',
-      description: 'Título da seção de trilha ágil.',
-    },
-    {
-      name: 'agileTrailSectionDescription',
-      type: 'string',
-      title: 'Descrição da seção de trilha ágil',
-      description: 'Descrição da seção de trilha ágil.',
-    },
-    {
-      name: 'agileTrailSectionButtonLabel',
-      type: 'string',
-      title: 'Label do botão da seção de trilha ágil',
-      description: 'Label do botão da seção de trilha ágil.',
-    },
-    {
-      name: 'popcornIndicationSectionTitle',
-      type: 'string',
-      title: 'Título da seção Pipoca Indica',
-      description: 'Título da seção Pipoca Indica.',
-    },
-    {
-      name: 'popcornIndicationSectionDescription',
-      type: 'string',
-      title: 'Descrição da seção Pipoca Indica',
-      description: 'Descrição da seção Pipoca Indica.',
-    },
-    {
-      name: 'popcornIndicationSectionButtonLabel',
-      type: 'string',
-      title: 'Label do botão da seção Pipoca Indica',
-      description: 'Label do botão da seção Pipoca Indica.',
-    },
-    {
-      name: 'newsletterSectionTitle',
-      type: 'string',
-      title: 'Título da seção de newsletter',
-      description: 'Título da seção de newsletter.',
-    },
-    {
-      name: 'newsletterSectionDescription',
-      type: 'string',
-      title: 'Descrição da seção de newsletter',
-      description: 'Descrição da seção de newsletter.',
-    },
-    {
-      name: 'newsletterSectionButtonLabel',
-      type: 'string',
-      title: 'Label do botão da seção de newsletter',
-      description: 'Label do botão da seção de newsletter.',
+      name: 'faqTopics',
+      type: 'array',
+      title: 'Perguntas e respostas do painel "Perguntas frequentes"',
+      description: 'Perguntas e respostas do painel "Perguntas frequentes"',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'question',
+              type: 'string',
+              title: 'Pergunta',
+              description: 'Pergunta.',
+            },
+            {
+              name: 'answer',
+              type: 'string',
+              title: 'Resposta',
+              description: 'Resposta.',
+            },
+          ],
+        },
+      ],
     },
   ],
 }
